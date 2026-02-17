3fc12b36659badcdf37362695b7e31560b7143fe
Lesley Lai <lesley@lesleylai.info>
2026-02-17 19:55:18 +0800

    Change site description

--- contents/_data/metadata.js
+++ contents/_data/metadata.js
@@ -1,5 +1,6 @@
 export default {
   siteTitle: "Graphics Programming Virtual Meetup",
   url: "https://gpvm.org/",
-  description: "Website for Graphics Programming Virtual Meetup",
+  description:
+    "Welcome to Graphics Programming Virtual Meetup, a weekly virtual meetup dedicated to graphics programming!",
 };
